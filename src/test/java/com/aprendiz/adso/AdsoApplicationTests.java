828545bb087d39cca37a31742b140547ea37a08e
Sennova <sennova@MacBook-Pro-de-Sennova.local>
2026-09-08 20:30:13 -0500

    Inicio Proyecto

---
+++ src/test/java/com/aprendiz/adso/AdsoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aprendiz.adso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdsoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
